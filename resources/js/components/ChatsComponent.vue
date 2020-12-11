bf268ec6f0abfb71c58dbc7581ef511e4c858715
mrusama1002 <mr.usamakhan1002@gmail.com>
2020-12-11 11:32:59 +0500

    whisper typing

--- resources/js/components/ChatsComponent.vue
+++ resources/js/components/ChatsComponent.vue
@@ -5,7 +5,7 @@
                 <div class="card card-default">
                     <div class="card-header">Messages</div>
                     <div class="card-body p-0">
-                        <ul class="list-unstyled" style="height: 300px; overflow-y: scroll;">
+                        <ul class="list-unstyled" style="height: 300px; overflow-y: scroll;" v-chat-scroll>
                             <li class="p-2" v-for="(message, index) in messages" :key="index">
                                 <strong>{{ message.user.name }}</strong>
                                 {{ message.message }}
@@ -13,6 +13,7 @@
                         </ul>
                     </div>
                     <input
+                        @keyup="sendTypingEvent"
                         @keyup.enter="sendMessage"
                         v-model="newMessage"
                         type="text"
@@ -21,7 +22,7 @@
                         placeholder="Enter your message...">
 
                 </div>
-                <span class="text-muted">user is typing...</span>
+                <span class="text-muted" v-if="activeUser">{{ activeUser.name }} is typing...</span>
             </div>
 
             <div class="col-4">
@@ -46,7 +47,9 @@
             return {
                 messages: [],
                 newMessage: '',
-                users: []
+                users: [],
+                activeUser: false,
+                typingTimer: false
             }
         },
 
@@ -67,6 +70,17 @@
             .listen('MessageSent', (event) => {
                 this.messages.push(event.message)
             })
+            .listenForWhisper('typing', user => {
+                this.activeUser = user;
+
+                if (this.typingTimer){
+                    clearTimeout(this.typingTimer)
+                }
+
+                this.typingTimer = setTimeout(() => {
+                    this.activeUser = false
+                }, 3000)
+            })
         },
 
         methods: {
@@ -83,6 +97,11 @@
                 });
                 axios.post('messages', {message: this.newMessage})
                 this.newMessage = '';
+            },
+
+            sendTypingEvent() {
+                Echo.join('chat')
+                    .whisper('typing', this.user);
             }
         }
     }
